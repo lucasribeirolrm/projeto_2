94aab07e46ac4a57c6da988a508f290d78268021
Lucas RM <110991424+lucasribeirolrm@users.noreply.github.com>
2024-04-09 22:54:09 -0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,12 +5,12 @@ from selenium import webdriver
 @pytest.fixture(scope="module")
 def browser():
     # Caminho para o executável do Chrome
-    chrome_path = "/home/lucasrm/dash/chrome-linux64/chrome"  # Substitua pelo caminho correto do seu Chrome
+    chrome_path = "/usr/bin/chromium-browser"  # Substitua pelo caminho correto do seu Chrome
 
     # Configurar o WebDriver para usar o Chrome
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = chrome_path
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=chrome_options)
     
     yield driver
     
