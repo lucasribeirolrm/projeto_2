322471ceb44580a6880e560c3b38e11cb3a4ba9a
Lucas RM <110991424+lucasribeirolrm@users.noreply.github.com>
2024-04-09 23:20:15 -0300

    Update test_home.py

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,5 +1,7 @@
+import time
 def test_home_page(browser):
     browser.get('http://127.0.0.1:8081')
+    time.sleep(5)
     assert browser.title == 'Dash'
     print("Teste da página inicial concluído com sucesso.")
 
