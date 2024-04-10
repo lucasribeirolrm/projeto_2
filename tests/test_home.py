81931014987f7f1a6fb64a736302695394acc920
Lucas RM <110991424+lucasribeirolrm@users.noreply.github.com>
2024-04-09 23:17:52 -0300

    Update test_home.py

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,8 +1,14 @@
-import time
-
 def test_home_page(browser):
-    url = "http://127.0.0.1:8080"  # Substitua pelo endereço correto da sua aplicação
-    browser.get(url)
-    time.sleep(5)
-    assert browser.title == "Dash"
+    browser.get('http://127.0.0.1:8081')
+    assert browser.title == 'Dash'
     print("Teste da página inicial concluído com sucesso.")
+
+
+#import time
+
+#def test_home_page(browser):
+    #url = "http://127.0.0.1:8080"  # Substitua pelo endereço correto da sua aplicação
+    #browser.get(url)
+    #time.sleep(5)
+    #assert browser.title == "Dash"
+    #print("Teste da página inicial concluído com sucesso.")
