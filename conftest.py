d1a0590fc764191d5fa46c572a057af214e4faca
Lucas RM <110991424+lucasribeirolrm@users.noreply.github.com>
2024-04-09 23:15:51 -0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,20 +1,43 @@
 import pytest
-import time
 from selenium import webdriver
 
 @pytest.fixture(scope="module")
 def browser():
+    # Simular configuração do navegador
+    # Por exemplo, configurando opções do Chrome
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')  # Executar sem interface gráfica
+    chrome_options.add_argument('--disable-gpu')  # Evitar erro ao rodar em headless mode
+
+    # Criar instância do WebDriver
+    driver = webdriver.Chrome(options=chrome_options)
+
+    yield driver  # Disponibilizar o WebDriver para os testes
+
+    # Fechar o navegador ao final dos testes
+    driver.quit()
+
+
+
+
+
+#import pytest
+#import time
+#from selenium import webdriver
+
+#@pytest.fixture(scope="module")
+#def browser():
     # Caminho para o executável do Chrome
-    chrome_path = "/usr/bin/chromium-browser"  # Substitua pelo caminho correto do seu Chrome
+    #chrome_path = "/usr/bin/chromium-browser"  # Substitua pelo caminho correto do seu Chrome
 
     # Configurar o WebDriver para usar o Chrome
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = chrome_path
-    driver = webdriver.Chrome(options=chrome_options)
+    #chrome_options = webdriver.ChromeOptions()
+    #chrome_options.binary_location = chrome_path
+    #driver = webdriver.Chrome(options=chrome_options)
     
-    yield driver
+    #yield driver
     
     # Fechar o navegador
-    driver.quit()
+    #driver.quit()
 
 
